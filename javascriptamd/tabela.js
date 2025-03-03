85adae01435cbda4171ecf9fdaefc9763afeb223
Caio <ce.costa@gmail.com>
2025-03-03 19:09:21 -0300

    centraz

--- javascriptamd/tabela.js
+++ javascriptamd/tabela.js
@@ -10,3 +10,15 @@ function filtrarTabela() {
         }
     });
 }
+
+function filtraTabelaVideo(){
+    let filtro_video = document.getElementById("filtro_video").value
+    let linha_2 = document.querySelectorAll("tbody tr");
+    linhas.forEach(linha => {
+        if (filtro_video === "todos" || linha.classList.contains(filtro_video)) {
+            linha_2.style.display = "";
+        } else {
+            linha_2.style.display = "none";
+        }
+    });
+}
